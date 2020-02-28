26f5857f0f1e60a27b56bfce725b1f002974ede0
AwrenNuit <a.rennaker@yahoo.com>
2020-02-28 17:01:53 -0600

    set up methods and click events

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -7,6 +7,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
+  currentNumber = '0';
+  firstOperand = null;
+  operator = null;
+  waitForSecondNumber = false;
+
+  public getNumber(v: string){
+    if(this.waitForSecondNumber){
+      this.currentNumber = v;
+      this.waitForSecondNumber = false;
+    }
+    else {
+      if(this.currentNumber === '0'){
+        this.currentNumber = v;
+      }
+      else {
+        this.currentNumber += v;
+      }
+    }
+  }
+
+  public getDecimal(){
+    if(!this.currentNumber.includes('.')){
+      this.currentNumber += '.';
+    }
+  }
+
+  private doCalculation(op1, op2){
+    switch(op1){
+      case '+':
+        return this.firstOperand += op2;
+      case '-':
+        return this.firstOperand -= op2;
+      case '*':
+        return this.firstOperand *= op2;
+      case '/':
+        return this.firstOperand /= op2;
+      case '=':
+        return op2;
+    }
+  }
+
+  public getOperation(op: string){
+    if(this.firstOperand === null){
+      this.firstOperand = +this.currentNumber;
+    }
+    else if(this.operator){
+      const result = this.doCalculation(this.operator, +this.currentNumber);
+      this.currentNumber = String(result);
+      this.firstOperand = result;
+    }
+    this.operator = op;
+    this.waitForSecondNumber = true;
+  }
+  
+  public clear(){
+    this.currentNumber = '0';
+    this.firstOperand = null;
+    this.operator = null;
+    this.waitForSecondNumber = false;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
